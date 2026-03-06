4115575ad6cb65de2632a9e04a064ae3b79ccba4
bokyoungp <bokyoungp@gmail.com>
2026-03-06 10:37:41 +0900

    person exercise

--- src/ch06/UpcastingDemo.java
+++ src/ch06/UpcastingDemo.java
@@ -3,11 +3,8 @@ package ch06;
 public class UpcastingDemo {
 
 	public static void main(String[] args) {
-		Circle1[] circles = new Circle1[3];
-		circles[0] = new Circle1();
-
-		Triangle[] triangles = new Triangle[3];
-		triangles[0] = new Triangle();
+		Circle1 circle = new Circle1();
+		Triangle triangle = new Triangle();
 
 		Shape[] shapes = new Shape[3];
 		shapes[0] = new Shape();
@@ -16,6 +13,32 @@ public class UpcastingDemo {
 
 		for (Shape shape : shapes) {
 			shape.area();
+		}
+
+		drawShape(circle);
+		drawShape(triangle);
+
+	}
+
+//	static void drawShape(Circle1 circle) {
+//		System.out.println("원을 그립니다.");
+//	}
+//	
+//	static void drawShape(Triangle triangle) {
+//		System.out.println("삼각형을 그립니다.");
+//	}
+
+	static void drawShape(Shape shape) {
+		System.out.println("도형을 그립니다.");
+		// instanceof Circle1 원의 경우에는 반지름을 설정한 후 원을 그린다.
+		// 삼각형이라면 밑변과 높이를 설정한 후 삼각형을 그린다.
+		if (shape instanceof Circle1 c) {
+			c.setRadius(5);
+			c.draw();
+		} else if (shape instanceof Triangle t) {
+			t.setBase(5);
+			t.setHeight(10);
+			t.draw();
 		}
 	}
 
@@ -36,11 +59,20 @@ class Circle1 extends Shape {
 	private int radius;
 
 	// 접근자, 설정자, toString
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
 	// 메서드 - 오버라이드
 	@Override
 	public void area() {
 		System.out.println("원의 넓이");
 	}
+
 }
 
 class Triangle extends Shape {
@@ -49,9 +81,26 @@ class Triangle extends Shape {
 	private int height;
 
 	// 접근자, 설정자, toString
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	// 메서드 - 오버라이드
 	@Override
 	public void area() {
 		System.out.println("삼각형의 넓이");
 	}
+
 }
--- src/exercise/ch06/Ex3.java
+++ src/exercise/ch06/Ex3.java
@@ -4,6 +4,12 @@ public class Ex3 {
 
 	public static void main(String[] args) {
 		// 사람, 학생, 외국학생을 각각 원소로 하는 배열선언하고 for-each 로 각 내용을 출력하라.
+		Person[] persons = new Person[3];
+		persons[0] = new Person(22, "길동이");
+		persons[1] = new Student(23, "황진이", "100");
+
+		persons[0].show();
+		persons[1].show();
 
 	}
 
@@ -11,24 +17,79 @@ public class Ex3 {
 
 class Person {
 	// 필드 - 이름, 나이
+	private int age;
+	private String name;
 
-	// 메서드 - 접근자와 생성자, show()
+	// 메서드 - 접근자와 설정자, show()
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void show() {
+		System.out.println("사람[이름:" + name + ", 나이 : " + age + "]");
+	}
 
 	// 생성자 - 모든 필드를 초기화하는 생성자
+	public Person(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
 }
 
 class Student extends Person {
 	// 필드 - 학번
+	private String studentNo;
+	// 메서드 - 접근자와 설정자, show()
 
-	// 메서드 - 접근자와 생성자, show()
+	public String getStudentNo() {
+		return studentNo;
+	}
+
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+
+	@Override
+	public void show() {
+		System.out.println("사람[이름:" + super.getName() + ", 나이 : " + super.getAge() + ", 학번 : " + studentNo + "]");
+	}
 
 	// 생성자 - 모든 필드를 초기화하는 생성자
+	public Student(int age, String name, String studentNo) {
+		super(age, name);
+		this.studentNo = studentNo;
+	}
 }
 
 class ForeignStudent extends Student {
 	// 필드 - 국적
+	private String nationality;
+	// 메서드 - 접근자와 설정자, show()
 
-	// 메서드 - 접근자와 생성자, show()
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
 
 	// 생성자 - 모든 필드를 초기화하는 생성자
+	public ForeignStudent(int age, String name, String studentNo, String nationality) {
+		super(age, name, studentNo);
+		this.nationality = nationality;
+	}
+
 }
