37a98b5763697cd13e378adc2ddb289d6d62e98e
bokyoungp <bokyoungp@gmail.com>
2026-03-13 14:12:02 +0900

    hashmap

--- src/ch11/MapDemo2.java
+++ src/ch11/MapDemo2.java
@@ -1,10 +1,27 @@
 package ch11;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapDemo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Map<Fruit, Integer> map = new HashMap<>();
 
+		map.put(k1, v1);
+		map.put(k1, v2);
+		map.put(null, 3);
+
+		System.out.println(map.size());
+
+		System.out.println(map);
 	}
 
+}
+
+class Fruit {
+	String name;
+	// 생성자
+
+	// 관련 메서드(hashcode, equals, toString)
 }
