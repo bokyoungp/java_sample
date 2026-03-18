b9c4b0825c3197ac452c547b004d5e4bfb16db80
bokyoungp <bokyoungp@gmail.com>
2026-03-18 12:31:34 +0900

    echo1server

--- src/ch18/SimpleClient.java
+++ src/ch18/SimpleClient.java
@@ -7,12 +7,13 @@ import java.net.Socket;
 public class SimpleClient {
 
 	public static void main(String[] args) {
-		try (Socket client = new Socket("localhost", 5000);
+		try (Socket client = new Socket("192.168.0.37", 5000);
 				OutputStream os = client.getOutputStream();
 				ObjectOutputStream oos = new ObjectOutputStream(os);) {
 
-			oos.writeObject("안녕, 단순 서버야");
+			oos.writeObject("안녕, 혜진 서버야");
 			oos.flush();
+			Thread.sleep(500);
 		} catch (Exception e) {
 		}
 
