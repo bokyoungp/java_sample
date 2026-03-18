bfe9b05242e7f901961a47ffa15182d3c915d3fb
bokyoungp <bokyoungp@gmail.com>
2026-03-18 14:15:56 +0900

    echo2server

--- src/ch18/Echo2Server.java
+++ src/ch18/Echo2Server.java
@@ -1,10 +1,50 @@
 package ch18;
 
-public class Echo2Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Echo2Server extends Thread {
 
+	protected static boolean cont = true;
+	protected Socket connection = null;
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = null;
+		server = new ServerSocket(5000);
+		System.out.println("서버 소켓 생성");
+
+		while (cont) {
+			System.out.println("연결 대기 중.....");
+			new Echo2Server(server.accept());
+		}
+		server.close();
+	}
+
+	private Echo2Server(Socket clientSocket) {
+		connection = clientSocket;
+		start(); // 새롭게 생성된 Thread 객체를 start()
+	}
+
+	public void run() { // Thread 가 실행할 코드
+		BufferedReader in;
+
+		System.out.println("클라이언트와 통신을 위한 새로운 스레드 생성");
+		try {
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			String msg;
+
+			while ((msg = in.readLine()) != null) {
+				System.out.println("읽은 메시지 메아리 : " + msg);
+			}
+
+			in.close();
+			connection.close();
+		} catch (IOException e) {
+		}
 	}
 
 }
