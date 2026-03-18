44fce625520be232feb86db08768ae08716a561b
bokyoungp <bokyoungp@gmail.com>
2026-03-18 11:50:46 +0900

    socket - 2

--- src/ch18/SimpleClient.java
+++ src/ch18/SimpleClient.java
@@ -1,9 +1,20 @@
 package ch18;
 
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class SimpleClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try (Socket client = new Socket("localhost", 5000);
+				OutputStream os = client.getOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(os);) {
+
+			oos.writeObject("안녕, 단순 서버야");
+			oos.flush();
+		} catch (Exception e) {
+		}
 
 	}
 
--- src/ch18/SimpleServer.java
+++ src/ch18/SimpleServer.java
@@ -1,9 +1,22 @@
 package ch18;
 
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SimpleServer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try (ServerSocket server = new ServerSocket(5000);
+				Socket connection = server.accept();
+				InputStream is = connection.getInputStream();
+				ObjectInputStream ois = new ObjectInputStream(is);) {
+
+			String str = (String) ois.readObject();
+			System.out.println("받은 문자열 = " + str);
+		} catch (Exception e) {
+		}
 
 	}
 
